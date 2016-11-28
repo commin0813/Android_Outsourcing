1bae04341ff452e52835e674a2fd9fda200991c3
commin0813 <commin0813@gmail.com>
2016-11-28 16:53:57 +0900

    first commit

---
+++ workspace/ExerciseProject/app/src/main/java/com/commin/pro/exerciseproject/Application.java
@@ -0,0 +1,13 @@
+package com.commin.pro.exerciseproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.application_layout);
+    }
+}

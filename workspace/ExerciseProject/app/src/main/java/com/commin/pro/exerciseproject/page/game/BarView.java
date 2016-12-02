1df97fbffef296cbdcc312defc513c11c1a615aa
commin0813 <commin0813@gmail.com>
2016-12-02 13:32:49 +0900

    game 완료

--- workspace/ExerciseProject/app/src/main/java/com/commin/pro/exerciseproject/page/game/BarView.java
+++ workspace/ExerciseProject/app/src/main/java/com/commin/pro/exerciseproject/page/game/BarView.java
@@ -4,38 +4,61 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.Date;
 
 /**
  * Created by user on 2016-12-02.
  */
-public class BarView extends View{
-    Context context;
-    public BarView(Context context) {
+public class BarView extends View {
+    private float width = 50.0f;
+    private float X = 0;
+    private float X_END = 0;
+    private float device_width = 0;
+    private int count = 0;
+    private Paint mPaint;
+
+    public BarView(Context context, float device_width) {
         super(context);
-        this.context = context;
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStrokeWidth(width);
+        this.device_width = device_width;
+        this.X_END = device_width;
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawColor(Color.BLUE);
-        Paint paint=new Paint();
+        if (X_END >= 2) {
 
-        paint.setColor(Color.CYAN);
-        paint.setStrokeWidth(100.0f);
-        paint.setTextSize(100.0f);
-
-        canvas.drawPoint(100,100,paint);
-        canvas.drawText("ddddd",0,0,paint);
-
-
+            if (X_END <= 100.0f) {
+                X_END = X_END - count * 0.1f;
+                mPaint.setColor(Color.RED);
+            } else if (X_END <= device_width / 2) {
+                X_END = X_END - count * 0.2f;
+                mPaint.setColor(Color.YELLOW);
+            } else {
+                X_END = X_END - count * 0.5f;
+                mPaint.setColor(Color.GREEN);
+            }
+            canvas.drawLine(X, getHeight() / 2, X_END, getHeight() / 2, mPaint);
+        } else {
+            X_END = device_width;
+            count = 0;
+        }
+        count++;
     }
 
-    public interface EventDraw {
-       void onScreenClick(Date date);
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                invalidate();
+                break;
+        }
+        return true;
     }
 }
--- workspace/ExerciseProject/app/src/main/java/com/commin/pro/exerciseproject/page/game/Page2Game.java
+++ workspace/ExerciseProject/app/src/main/java/com/commin/pro/exerciseproject/page/game/Page2Game.java
@@ -1,8 +1,11 @@
 package com.commin.pro.exerciseproject.page.game;
 
+import android.graphics.Canvas;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -12,9 +15,10 @@ import com.commin.pro.exerciseproject.R;
 import com.commin.pro.exerciseproject.page.photo.Page2Photo;
 
 public class Page2Game extends AppCompatActivity {
-    TextView tv_game_title;
-    LinearLayout ll_container;
-    BarView barView;
+    private TextView tv_game_title;
+    private LinearLayout ll_container;
+    private BarView barView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +32,15 @@ public class Page2Game extends AppCompatActivity {
     }
 
     private void init_elements() {
-        barView = new BarView(Page2Game.this);
-//
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels;
+
+        barView = new BarView(Page2Game.this, Float.valueOf(width));
         tv_game_title = (TextView) findViewById(R.id.tv_game_title);
         tv_game_title.setSelected(true);
-//
         ll_container = (LinearLayout) findViewById(R.id.ll_container);
         barView.setVisibility(View.VISIBLE);
-        ll_container.addView(barView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        ll_container.addView(barView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
     }
 }

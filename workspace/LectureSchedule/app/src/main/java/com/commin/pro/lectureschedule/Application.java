cec63de6299dccb9d9f364762ab9ade6e3f5ddb4
commin0813 <commin0813@gmail.com>
2016-12-13 22:57:25 +0900

    aa

--- workspace/LectureSchedule/app/src/main/java/com/commin/pro/lectureschedule/Application.java
+++ workspace/LectureSchedule/app/src/main/java/com/commin/pro/lectureschedule/Application.java
@@ -9,5 +9,7 @@ public class Application extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_application);
+
+        //dkssud
     }
 }

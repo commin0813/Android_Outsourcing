0f5a7a0b9583132bbc87971df0fc8ca5f9a2e87d
commin0813 <commin0813@gmail.com>
2016-12-14 15:39:26 +0900

    sample

---
+++ workspace/MyApplication2222/app/src/main/java/com/commin/pro/myapplication2222/demo/WeekSets.java
@@ -0,0 +1,10 @@
+package com.commin.pro.myapplication2222.demo;
+
+public class WeekSets {
+	public String day;
+	public String jobID;
+	public String jobRefID;
+	public String tapMargin;
+	public String buttonHight;
+
+}

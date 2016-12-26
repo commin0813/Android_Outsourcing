b2473c7ef3e172173c153194b7bfe166d035f5c8
commin0813 <commin0813@gmail.com>
2016-12-26 10:13:19 +0900

    calendar

---
+++ workspace/ComminCalendar/app/src/main/java/com/commin/pro/commincalendar/Application.java
@@ -0,0 +1,13 @@
+package com.commin.pro.commincalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_application);
+    }
+}

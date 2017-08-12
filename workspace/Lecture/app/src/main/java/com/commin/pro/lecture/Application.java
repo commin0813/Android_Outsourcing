fb0b3661172d1d61b4dd4b7f3d5ba002da6e6dd0
commin0813 <commin0813@gmail.com>
2017-08-12 17:05:37 +0900

    qqqq

--- workspace/Lecture/app/src/main/java/com/commin/pro/lecture/Application.java
+++ workspace/Lecture/app/src/main/java/com/commin/pro/lecture/Application.java
@@ -2,6 +2,7 @@ package com.commin.pro.lecture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Application extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Application extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+
+        Log.d("aa","");
     }
 }
